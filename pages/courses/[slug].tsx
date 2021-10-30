fed1b8949679d0e6a7758d9bf139e3e6c68307a9
Vitaliy Inshakov <michael8913@gmail.com>
2021-10-30 13:26:58 +0300

    course hero

--- pages/courses/[slug].tsx
+++ pages/courses/[slug].tsx
@@ -1,15 +1,19 @@
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import { NextPageWithLayout } from "@pages/_app";
 import { Modal } from "@components/common";
 import { CourseHero, Curriculum, Keypoints } from "@components/course";
 import { BaseLayout } from "@components/layout";
 import { getAllCourses } from "@content/courses/fetcher";
 
-const Course: NextPageWithLayout = () => {
+const Course: NextPageWithLayout = ({ course }: InferGetStaticPropsType<typeof getStaticProps>) => {
 	return (
 		<>
 			<div className="py-4">
-				<CourseHero />
+				<CourseHero
+					title={course.title}
+					description={course.description}
+					image={course.coverImage}
+				/>
 			</div>
 			<Keypoints />
 			<Curriculum />
