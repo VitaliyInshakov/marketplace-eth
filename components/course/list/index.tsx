85f1d16589c8dfb932fd398892ba2ee113a96192
Vitaliy Inshakov <michael8913@gmail.com>
2021-10-30 13:04:43 +0300

    course details

--- components/course/list/index.tsx
+++ components/course/list/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { CourseType } from "@content/courses/fetcher";
 
 type ListProps = {
@@ -8,7 +9,7 @@ type ListProps = {
 export default function List({ courses }: ListProps) {
 	return (
 		<section className="grid md:grid-cols-1 lg:grid-cols-2 gap-4 mb-5">
-			{courses.map(({ id, title, coverImage, type, description }) =>
+			{courses.map(({ id, title, coverImage, type, description, slug }) =>
 				<div key={id} className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
 					<div className="flex h-full">
 						<div className="flex h-full">
@@ -25,10 +26,10 @@ export default function List({ courses }: ListProps) {
 							<div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
 								{type}
 							</div>
-							<a
-								href="#"
-								className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-							>{title}</a>
+							<Link href={`/courses/${slug}`}>
+								<a className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+								>{title}</a>
+							</Link>
 							<p className="mt-2 text-gray-500">
 								{description}
 							</p>
