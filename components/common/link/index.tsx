973ca26f5a782c5ace26966fc15e4c30c37e8391
Vitaliy Inshakov <michael8913@gmail.com>
2021-11-24 00:09:04 +0200

    components change

--- components/common/link/index.tsx
+++ components/common/link/index.tsx
@@ -9,7 +9,7 @@ type ActiveLink = {
 } & LinkProps;
 export default function ActiveLink({ children, activeLinkClass, ...props }: ActiveLink) {
     const { pathname } = useRouter();
-    let className = children?.props.className || "";
+    let className = (children as any).props.className || "";
 
     if (pathname === props.href) {
         className = `${className} ${activeLinkClass ? activeLinkClass : "text-indigo-600"}`
--- pages/marketplace/courses/owned.tsx
+++ pages/marketplace/courses/owned.tsx
@@ -1,14 +1,21 @@
 import { OwnedCourseCard } from "@components/course";
 import { BaseLayout } from "@components/layout";
 import { MarketHeader } from "@components/marketplace";
+import { Button, Message } from "@components/common";
 
 export default function OwnedCourses() {
-
     return (
         <>
             <MarketHeader />
             <section className="grid grid-cols-1">
-                <OwnedCourseCard />
+                <OwnedCourseCard>
+                    <Message>
+                        My custom message!
+                    </Message>
+                    <Button>
+                        Watch the course
+                    </Button>
+                </OwnedCourseCard>
             </section>
         </>
     )
