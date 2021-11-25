96e90d5418aea3a1992d32b57d1596c25e3b9297
Vitaliy Inshakov <michael8913@gmail.com>
2021-11-25 23:02:40 +0200

    complete contract implement

--- contracts/CourseMarketplace.sol
+++ contracts/CourseMarketplace.sol
@@ -11,19 +11,41 @@ contract CourseMarketplace {
     struct Course {
         uint id;
         uint price;
-        byte32 proof;
+        bytes32 proof;
         address owner;
         State state;
     }
-
 
     mapping(bytes32 => Course) private ownedCourses;
     mapping(uint => bytes32) private ownedCourseHash;
 
     uint private totalOwnedCourses;
+    address payable private owner;
+
+    constructor() {
+        setContractOwner(msg.sender);
+    }
+
+    /// Course has already a owner!
+    error CourseHasOwner();
+
+    /// Only owner has an access!!
+    error OnlyOwner();
+
+    modifier onlyOwner() {
+        if (msg.sender != getContractOwner()) {
+            revert OnlyOwner();
+        }
+        _;
+    }
 
     function purchaseCourse(bytes16 courseId, bytes32 proof) external payable {
         bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+
+        if (hasCourseOwnership(courseHash)) {
+            revert CourseHasOwner();
+        }
+
         uint id = totalOwnedCourses++;
         ownedCourseHash[id] = courseHash;
         ownedCourses[courseHash] = Course({
@@ -33,6 +55,10 @@ contract CourseMarketplace {
             owner: msg.sender,
             state: State.Purchased
         });
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        setContractOwner(newOwner);
     }
 
     function getCourseCount() external view returns(uint) {
@@ -45,5 +71,17 @@ contract CourseMarketplace {
 
     function getCourseByHash(bytes32 hash) external view returns(Course memory) {
         return ownedCourses[hash];
+    }
+
+    function getContractOwner() public view returns (address) {
+        return owner;
+    }
+
+    function setContractOwner(address newOwner) private {
+        owner = payable(newOwner);
+    }
+
+    function hasCourseOwnership(bytes32 courseHash) private view returns (bool) {
+        return ownedCourses[courseHash].owner == msg.sender;
     }
 }
